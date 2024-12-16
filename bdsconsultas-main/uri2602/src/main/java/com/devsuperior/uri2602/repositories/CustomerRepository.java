c5aa7b09981bcd69a41b00f4a3357d329ee8ae63
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-12-16 13:45:30 -0300

    Repositories 2602

---
+++ bdsconsultas-main/uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.devsuperior.uri2602.repositories;
+
+import com.devsuperior.uri2602.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    
+
+
+}
