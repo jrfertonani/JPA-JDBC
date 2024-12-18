1af2c4b35f9652967602617f4ebc0f787f581c9e
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-12-18 20:35:08 -0300

    customerRepositories 2602

--- bdsconsultas-main/uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
+++ bdsconsultas-main/uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
@@ -1,13 +1,20 @@
 package com.devsuperior.uri2602.repositories;
 
 import com.devsuperior.uri2602.entities.Customer;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-
+     @Query(nativeQuery = true, value = "SELECT name "
+             + "FROM customers "
+             + "where state= :state")
+     List<CustomerMinProjection> search1(String state);
 
 
 }
