64d7ffd8a1b427190e218ce706b2f7408e1cd33d
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-12-19 11:44:56 -0300

    Projections 2621

--- bdsconsultas-main/uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ bdsconsultas-main/uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -3,7 +3,12 @@ package com.devsuperior.uri2621.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.uri2621.entities.Product;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+
 
 }
