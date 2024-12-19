533faed3623a6a9cf288fe7957a41dfeb8d2a014
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-12-19 11:55:09 -0300

    Application 2621

--- bdsconsultas-main/uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
+++ bdsconsultas-main/uri2621/src/main/java/com/devsuperior/uri2621/Uri2621Application.java
@@ -1,5 +1,6 @@
 package com.devsuperior.uri2621;
 
+import com.devsuperior.uri2621.dto.ProductMinDTO;
 import com.devsuperior.uri2621.projections.ProductMinProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.devsuperior.uri2621.repositories.ProductRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2621Application implements CommandLineRunner {
@@ -23,5 +25,14 @@ public class Uri2621Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		List<ProductMinProjection> list = repository.search1(10,20,"P");
+		List<ProductMinDTO> result1 = list.stream().map(x -> new ProductMinDTO(x)).collect(Collectors.toList());
+
+		System.out.println("\n****Consulta nativeQuery" );
+		for(ProductMinDTO obj : result1){
+			System.out.println(obj);
+		}
+
+		System.out.println("\n\n");
+
 	}
 }
