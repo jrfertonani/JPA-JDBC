841ff9a68f643250a8430f0a49c30dca7ce73096
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-12-19 11:58:58 -0300

    JPQL 2621

--- bdsconsultas-main/uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ bdsconsultas-main/uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.uri2621.repositories;
 
+import com.devsuperior.uri2621.dto.ProductMinDTO;
 import com.devsuperior.uri2621.projections.ProductMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -19,6 +20,12 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             + "WHERE products.amount BETWEEN :min AND :max "
             + "AND providers.name LIKE CONCAT( :beginName, '%')")
     List<ProductMinProjection> search1(Integer min, Integer max, String beginName );
+
+    @Query(          "SELECT new com.devsuperior.uri2621.dto.ProductMinDTO(obj.name) "
+                    + "FROM Product obj "
+                    + "WHERE obj.amount BETWEEN :min AND :max "
+                    + "AND obj.provider.name LIKE CONCAT( :beginName, '%')")
+    List<ProductMinDTO> search2(Integer min, Integer max, String beginName );
 
 
 
