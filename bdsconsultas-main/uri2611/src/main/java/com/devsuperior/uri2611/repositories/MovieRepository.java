71fbc90d732d9aa6240f9f503a3a550f0724fff3
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-12-19 10:05:20 -0300

    Repository 2611

--- bdsconsultas-main/uri2611/src/main/java/com/devsuperior/uri2611/repositories/MovieRepository.java
+++ bdsconsultas-main/uri2611/src/main/java/com/devsuperior/uri2611/repositories/MovieRepository.java
@@ -3,6 +3,7 @@ package com.devsuperior.uri2611.repositories;
 import com.devsuperior.uri2611.entities.Movie;
 import com.devsuperior.uri2611.projection.MovieMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +11,11 @@ import java.util.List;
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
+    @Query(nativeQuery = true, value = "SELECT movies.id, movies.name "
+            + "FROM movies "
+            + "INNER JOIN genres ON movies.id_genres = genres.id "
+            + "WHERE genres.description = :genreName ")
     List<MovieMinProjection> search1(String genreName);
-    
+
 
 }
